c088e2bf1ac165e6d579d5436f6ecd4826599185
Alex Pasalic <pasalic.alex@gmail.com>
2018-11-08 13:24:59 +1000

    Modify the Prescription Class

--- PharmaCare/PharmaCare/Models/Prescriptions.cs
+++ PharmaCare/PharmaCare/Models/Prescriptions.cs
@@ -8,12 +8,16 @@ namespace PharmaCare.Models
     public class Prescriptions
     {
         public int PrescriptionID { get; set; }
-        public string PatientFname { get; set; }
-        public string PatientLname { get; set; }
-        public string Drugs { get; set; }
+        public string DrugID { get; set; }
+        public string PatientNames { get; set; }
+        public string PrescribingDate { get; set; }
+        public string PrescribingDotor { get; set; }
+        public string InformationExtra { get; set; }
+        public string StatusPrescription { get; set; }
         public string Doses { get; set; }
-        public string SugestionTakenDes { get; set; }
-        public string DoctorFname { get; set; }
-        public string DoctorLname { get; set; }
+        public string FirstTimeUse { get; set; }
+        public string LastTimeUse { get; set; }
+        public string FrequenseUseInADay { get; set; }
+        public string DoseStatus { get; set; }
     }
 }
