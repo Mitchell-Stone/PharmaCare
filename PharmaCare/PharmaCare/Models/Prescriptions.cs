980fc36d21617a083c59f62a744676e1c9aa6abb
Alex Pasalic <pasalic.alex@gmail.com>
2018-11-08 11:23:12 +1000

    created a prescription model

---
+++ PharmaCare/PharmaCare/Models/Prescriptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmaCare.Models
+{
+    public class Prescriptions
+    {
+        public int PrescriptionID { get; set; }
+        public string PatientFname { get; set; }
+        public string PatientLname { get; set; }
+        public string Drugs { get; set; }
+        public string Doses { get; set; }
+        public string SugestionTakenDes { get; set; }
+        public string DoctorFname { get; set; }
+        public string DoctorLname { get; set; }
+    }
+}
