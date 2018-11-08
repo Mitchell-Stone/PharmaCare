d3051c471f36709087aca38e4f159345bf8ca797
Saugat Raut <saugatraut97@gmail.com>
2018-11-08 15:01:18 +1000

    Design Fix

--- PharmaCare/PharmaCare/LogIn.aspx.designer.cs
+++ PharmaCare/PharmaCare/LogIn.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace PharmaCare {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnLogin;
+        
+        /// <summary>
+        /// btn_Signup control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Signup;
     }
 }
--- PharmaCare/PharmaCare/SignUp.aspx.designer.cs
+++ PharmaCare/PharmaCare/SignUp.aspx.designer.cs
@@ -20,5 +20,23 @@ namespace PharmaCare {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// btn_Back control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Back;
+        
+        /// <summary>
+        /// btn_Submit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Submit;
     }
 }
