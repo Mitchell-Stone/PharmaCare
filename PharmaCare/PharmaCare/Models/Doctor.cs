3940fc5a1a402b1fcbaca0a44e44e4ded1c148a8
Kaitlyn <kaitie.98@hotmail.com>
2018-11-08 11:59:21 +1000

    Added missing ; on doctor class

--- PharmaCare/PharmaCare/Models/Doctor.cs
+++ PharmaCare/PharmaCare/Models/Doctor.cs
@@ -12,6 +12,6 @@ namespace PharmaCare.Models
         public string Address { get; set; }
         public string City { get; set; }
         public string ZipCode { get; set; }
-        public string Speciality { get;set }
+        public string Speciality { get; set; }
     }
 }
