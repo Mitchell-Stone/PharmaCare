35ee742172de3438d6cbf6bed2541da3235dd75e
Alex Pasalic <pasalic.alex@gmail.com>
2018-11-29 09:58:16 +1000

    update the chart on opdpage

--- PharmaCare/PharmaCare/SearchPrescriptions.aspx.cs
+++ PharmaCare/PharmaCare/SearchPrescriptions.aspx.cs
@@ -1,8 +1,8 @@
-﻿/* 
+﻿/*          Date created = 6 Nov 2018
  *          Page Create By Hsuan-Yi Lin(a.k.a Alex Pasalic) 
- *
+ *          purpose = to present the view page function for out door patient for the staff
  *          Student ID= 452400286
- *
+ *          Known bugs= sorting function might not work in some of the fields.
  */
 using PharmaCare.Models;
 using System;
@@ -17,50 +17,52 @@ namespace PharmaCare
 
     public partial class SearchPrescriptions : System.Web.UI.Page
     {
-        List<Prescription> prescription = new List<Prescription>();
+        //List<Prescription> prescription = new List<Prescription>();
 
         protected void Page_Load(object sender, EventArgs e)
         {
             UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
-            if (!IsPostBack)
-            {
-               // PresentDataInTheList("All");
-            }
+            // if (!IsPostBack)
+            //  {
+            // PresentDataInTheList("All");
+            // }
         }
-        
+
         protected void BtnFindPatient_Click(object sender, EventArgs e)
         {
-            string search = txtSearchPatient.Text;
+            //string search = txtSearchPatient.Text;
+
 
         }
 
         protected void dgvPrescriptions_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+
         }
+
         /*private void PresentDataInTheList (string status)
 {
-   SqlConnection con = PharmaCareDB.GetConnection();
-   List<string> tempList = new List<string>();
+SqlConnection con = PharmaCareDB.GetConnection();
+List<string> tempList = new List<string>();
 
-   try
-   {
-       con.Open();
-       if (status == "All")
-       {
-           SqlDataReader reader = PrescriptionDB.GetODPprescription(con, status);
-           dgvPrescriptions.DataSource = reader;
-           dgvPrescriptions.DataBind();
-       }
-   }
-   catch (SqlException ex)
-   {
-       throw ex;
-   }
-   finally
-   {
-       con.Close();
-   }
+try
+{
+con.Open();
+if (status == "All")
+{
+  SqlDataReader reader = PrescriptionDB.GetODPprescription(con, status);
+  dgvPrescriptions.DataSource = reader;
+  dgvPrescriptions.DataBind();
+}
+}
+catch (SqlException ex)
+{
+throw ex;
+}
+finally
+{
+con.Close();
+}
 }*/
     }
 }
--- PharmaCare/PharmaCare/SearchPrescriptions.aspx.designer.cs
+++ PharmaCare/PharmaCare/SearchPrescriptions.aspx.designer.cs
@@ -13,42 +13,6 @@ namespace PharmaCare {
     public partial class SearchPrescriptions {
         
         /// <summary>
-        /// txtSearchPatient control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtSearchPatient;
-        
-        /// <summary>
-        /// btnFindPatient control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnFindPatient;
-        
-        /// <summary>
-        /// txtFindPrescription control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtFindPrescription;
-        
-        /// <summary>
-        /// btnFindPrescription control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnFindPrescription;
-        
-        /// <summary>
         /// dgvPrescriptions control.
         /// </summary>
         /// <remarks>
@@ -73,7 +37,7 @@ namespace PharmaCare {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DetailsView DetailsPrescription;
+        protected global::System.Web.UI.WebControls.GridView DetailsPrescription;
         
         /// <summary>
         /// SQLDrugDetails control.
